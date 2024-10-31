ad121ce3f74eab8c87b8c315f6d2a520caf1c953
Nikolai-Gluhove <nik.gluhov98@mail.ru>
2024-10-31 16:54:34 +0300

    2.4 Query params

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -2,11 +2,19 @@ import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HelloWorldTest {
 
     @Test
     public void testRestAssured(){
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "Nikolai");
+
         Response response = RestAssured
+                .given()
+                .queryParams(params)
                 .get("https://playground.learnqa.ru/api/hello")
                 .andReturn();
 
