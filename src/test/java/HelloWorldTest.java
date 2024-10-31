8fdd5b45693899ccc70e91f9afb9541bb0fe57ca
Nikolai-Gluhove <nik.gluhov98@mail.ru>
2024-10-31 23:13:33 +0300

    2.8 Headers

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,26 +1,30 @@
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
+import io.restassured.http.Headers;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 public class HelloWorldTest {
 
     @Test
     public void testRestAssured(){
+        Map<String, String> headers = new HashMap<>();
+        headers.put("muHeader1", "myValue1");
+        headers.put("muHeader2", "myValue2");
 
         Response response = RestAssured
                 .given()
                 .redirects()
-                .follow(true)
+                .follow(false)
                 .when()
                 .get("https://playground.learnqa.ru/api/get_303")
                 .andReturn();
 
-        int statusCode = response.getStatusCode();
-        System.out.println(statusCode);
+        response.prettyPrint();
+
+        String locationHeaders = response.getHeader("Location");
+        System.out.println(locationHeaders);
     }
 }
