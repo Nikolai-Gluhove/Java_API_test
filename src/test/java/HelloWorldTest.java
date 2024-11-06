ee0051b7a12a8e4fe1c455aff214acf668f6f9d4
Nikolai-Gluhove <nik.gluhov98@mail.ru>
2024-11-06 16:41:55 +0300

    ex2.9 Password

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,54 +1,69 @@
-
-import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
-import java.lang.Thread;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class HelloWorldTest {
 
     @Test
     public void testRestAssured() throws InterruptedException {
+        //Парроли
+        HashMap<Integer, String> password = new HashMap<>();
+        password.put(0, "123456");
+        password.put(1, "123456789");
+        password.put(2, "qwerty");
+        password.put(3, "password");
+        password.put(4, "1234567");
+        password.put(5, "12345678");
+        password.put(6, "iloveyou");
+        password.put(7, "111111");
+        password.put(8, "123123");
+        password.put(9, "abc123");
+        password.put(10, "qwerty123");
+        password.put(11, "1q2w3e4r");
+        password.put(12, "admin");
+        password.put(13, "qwertyuiop");
+        password.put(14, "654321");
+        password.put(15, "555555");
+        password.put(16, "lovely");
+        password.put(17, "7777777");
+        password.put(18, "welcome");
+        password.put(19, "888888");
+        password.put(20, "princess");
+        password.put(21, "dragon");
+        password.put(22, "password1");
+        password.put(23, "123qwe");
+        password.put(24, "12345");
 
-        JsonPath jsonFirstResponse = RestAssured
-                .given()
-                .when()
-                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
-                .jsonPath();
+        //параметры тела
 
-
-        String token = jsonFirstResponse.getString("token");
-        int seconds = jsonFirstResponse.getInt("seconds");
-
-        JsonPath jsonTest;
-        for (int i = 0; i < 2; i++) {
-            jsonTest = RestAssured
+        for (int i = 0; i < password.size(); i++) {
+            //запрос на получение авторизационного cookie
+            Response responseAutorization = RestAssured
                     .given()
-                    .queryParam("token", token)
                     .when()
-                    .get("https://playground.learnqa.ru/ajax/api/longtime_job")
-                    .jsonPath();
+                    .body("{\"login\":\"super_admin\", \"password\":\""+password.get(i)+"\"}")
+                    .post("https://playground.learnqa.ru/ajax/api/get_secret_password_homework")
+                    .andReturn();
 
-            String status = jsonTest.getString("status");
-            switch (i){
-                case 0:
-                    if (status.equals("Job is NOT ready")){
-                        System.out.println("Status is correct");
-                        Thread.sleep(seconds * 1000);
-                        break;
-                    }
-                    System.out.println("Status is NOT correct");
-                    Thread.sleep(seconds * 1000);
-                    break;
+            String autorizationCookie = responseAutorization.getCookie("auth_cookie");
 
-                case 1:
-                    if(status.equals("Job is ready")){
-                        System.out.println("Status is correct");
-                        break;
-                    }
-                    System.out.println("Status is NOT correct");
-                    break;
-            }
+            //запрос для проверки пароля
+           Response checkAutoriz = RestAssured
+                   .given()
+                   .when()
+                   .cookie("auth_cookie="+autorizationCookie)
+                   .post("https://playground.learnqa.ru/ajax/api/check_auth_cookie")
+                   .andReturn();
+
+           String conten = checkAutoriz.getBody().asString();
+           if (!conten.contains("You are NOT authorized")){
+               System.out.println("Password = "+autorizationCookie+" - "+conten);
+               break;
+           }
         }
     }
 }
