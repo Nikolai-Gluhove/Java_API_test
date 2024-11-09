9a2fcd33e193ba11b32383a4c9a245e4a2fe8067
Nikolai-Gluhove <nik.gluhov98@mail.ru>
2024-11-09 12:06:10 +0300

    ex10 Short phrase test

--- src/test/java/lib/Assertions.java
+++ src/test/java/lib/Assertions.java
@@ -4,6 +4,7 @@ import io.restassured.response.Response;
 
 import static org.hamcrest.Matchers.hasKey;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Assertions {
     public static void assertJsonByName(Response Response, String name, int expectedValue){
@@ -11,5 +12,9 @@ public class Assertions {
 
         int value = Response.jsonPath().getInt(name);
         assertEquals(expectedValue, value, "JSON value is not equal to expected value");
+    }
+
+    public static void assertStringLength(String message){
+        assertTrue(message.length() > 15, "Length message less 15 symbols");
     }
 }
