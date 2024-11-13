d6c0d70ed9a1c44ccbcc87e48c76d8b221c726f6
Nikolai-Gluhove <nik.gluhov98@mail.ru>
2024-11-13 16:54:23 +0300

    4.2 Create a new user

--- src/test/java/lib/Assertions.java
+++ src/test/java/lib/Assertions.java
@@ -28,4 +28,8 @@ public class Assertions {
         );
     }
 
+    public static void assertJsonHasKey(Response Response, String expectedFieldName){
+        Response.then().assertThat().body("$", hasKey(expectedFieldName));
+    }
+
 }
