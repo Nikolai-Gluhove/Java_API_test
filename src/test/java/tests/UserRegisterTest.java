4cfd3626dbec46e04e629605c29f06c71d081e84
Nikolai-Gluhove <nik.gluhov98@mail.ru>
2024-11-13 16:38:30 +0300

    4.1 Create an existing user

---
+++ src/test/java/tests/UserRegisterTest.java
@@ -0,0 +1,35 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import lib.Assertions;
+import lib.BaseTestCase;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserRegisterTest extends BaseTestCase {
+    @Test
+    public void testCreateUserWithExistingEmail(){
+        String email = "vinkotov@example.com";
+
+        Map<String, String> userDate = new HashMap<>();
+        userDate.put("email", email);
+        userDate.put("password", "123");
+        userDate.put("username", "learnqa");
+        userDate.put("firstName", "learnqa");
+        userDate.put("lastName", "learnqa");
+
+        Response responseCreateAuth = RestAssured
+                .given()
+                .body(userDate)
+                .when()
+                .post("https://playground.learnqa.ru/api/user/")
+                .andReturn();
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
+        Assertions.assertResponseTextEquals(responseCreateAuth, "Users with email '"+email+"' already exists");
+
+    }
+}
