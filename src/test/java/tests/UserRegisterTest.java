393be6ffd5dab26dff341ce0307ec41a3e9f4ba9
Nikolai-Gluhove <nik.gluhov98@mail.ru>
2024-11-18 21:23:18 +0300

    ex15 User method test

--- src/test/java/tests/UserRegisterTest.java
+++ src/test/java/tests/UserRegisterTest.java
@@ -1,16 +1,30 @@
 package tests;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import lib.ApiCoreRequests;
 import lib.Assertions;
 import lib.BaseTestCase;
 import lib.DateGenerator;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
+@Epic("Create user case")
+@Feature("Create user")
 public class UserRegisterTest extends BaseTestCase {
+
+    private final ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
+    Random random = new Random();
+
     @Test
     public void testCreateUserWithExistingEmail(){
         String email = "vinkotov@example.com";
@@ -45,4 +59,67 @@ public class UserRegisterTest extends BaseTestCase {
         Assertions.assertResponseCodeEquals(responseCreateAuth, 200);
         Assertions.assertJsonHasField(responseCreateAuth, "id");
     }
+
+    @Test
+    @Description("This test check create user without sing")
+    @DisplayName("Create user without sing")
+    public void testCreateUserWithoutSing(){
+       Map<String, String> userDate = new HashMap<>();
+       userDate.put("email", "learnqaexample.com");
+       userDate = DateGenerator.getRegistrationDate(userDate);
+
+       Response responseCreateUserWithoutSing = apiCoreRequests.makePostRequest("https://playground.learnqa.ru/api/user/", userDate);
+
+       Assertions.assertResponseTextEquals(responseCreateUserWithoutSing, "Invalid email format");
+    }
+
+    @DisplayName("Create user without param")
+    @Description("This test check create user without one param")
+    @ParameterizedTest()
+    @ValueSource(strings = {"username", "firstName", "lastName", "email", "password"})
+    public void testCreateUserWithoutParam(String param){
+        Map<String, String> userDate = DateGenerator.getRegistrationDate(param);;
+
+        Response responseWithoutParam = apiCoreRequests.makePostRequest("https://playground.learnqa.ru/api/user/", userDate);
+
+        Assertions.assertResponseTextEquals(responseWithoutParam, "The following required params are missed: "+param);
+    }
+
+
+    @DisplayName("Create a user with a short name")
+    @Description("This test check the create of a user whit a one-character name")
+    @Test
+    public void testCreateUserShortName(){
+        String firstName = String.valueOf((char) (random.nextInt(26) + 'a'));
+        Map<String, String> userDate = new HashMap<>();
+        userDate.put("firstName", firstName);
+        userDate = DateGenerator.getRegistrationDate(userDate);
+
+        Response responseWishShortName = apiCoreRequests.makePostRequest("https://playground.learnqa.ru/api/user/", userDate);
+
+        Assertions.assertResponseCodeEquals(responseWishShortName, 400);
+        Assertions.assertResponseTextEquals(responseWishShortName, "The value of 'firstName' field is too short");
+    }
+
+    @DisplayName("Create a user with a long name")
+    @Description("This test check the create of a user with a 250-character name")
+    @Test
+    public void testCreateWithLongName(){
+        String firstName = "";
+        for (int i = 0; i < 251; i++){
+            char ch = (char) (random.nextInt(26) + 'a');
+            firstName = firstName + ch;
+        }
+        Map<String, String> userDate = new HashMap<>();
+        userDate.put("firstName", firstName);
+        userDate = DateGenerator.getRegistrationDate(userDate);
+
+        Response responseWithLongName = apiCoreRequests.makePostRequest("https://playground.learnqa.ru/api/user/", userDate);
+
+        Assertions.assertResponseCodeEquals(responseWithLongName, 400);
+        Assertions.assertResponseTextEquals(responseWithLongName, "The value of 'firstName' field is too long");
+    }
+
 }
+
+
